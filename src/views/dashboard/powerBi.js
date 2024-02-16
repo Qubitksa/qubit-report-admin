4bb05b587f02c5603cb9f7636a3d449d2be5ff8b
Abdullah Al Zahrani <abdullahwaqet@gmail.com>
2024-02-16 20:22:20 +0300

    Update powerBi.js

--- src/views/dashboard/powerBi.js
+++ src/views/dashboard/powerBi.js
@@ -1,19 +1,19 @@
-import React from "react";
+import React from 'react';
 
 function PowerBi() {
-  return (
+ return (
     <div>
       <iframe
         width="860"
         height="484"
         src="https://app.powerbi.com/view?r=eyJrIjoiZWI1ZmE1MDgtYTdiNS00ODhhLWIyZmUtZWNjZmVmYTcwNWIxIiwidCI6Ijg0M2ZjZjIxLTFkMjUtNGI0Ny04N2JkLTlmYjRhOWU2OTFkNSIsImMiOjl9" 
         title="Introduction To WiseGPT"
-        frameborder="0"
+        frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         allowfullscreen
       ></iframe>
     </div>
-  );
+  )
 }
 
-export default PowerBi;
+export default PowerBi
